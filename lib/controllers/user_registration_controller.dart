40b1204def5f0019cd701ed8d1eac6b4d8d4047f
Bakhodir <106083101+Bakhodir95@users.noreply.github.com>
2024-07-18 00:12:47 +0500

    defde

--- lib/controllers/user_registration_controller.dart
+++ lib/controllers/user_registration_controller.dart
@@ -1,10 +1,57 @@
+import 'dart:io';
+
 import 'package:exam_project_with_providers/services/users_firbase_service.dart.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserRegistrationController with ChangeNotifier {
+  String _email = '';
+  String _name = '';
+  String _surname = '';
+  String _imageUrl = "";
+
+  String get email => _email;
+  String get name => _name;
+  String get surname => _surname;
+  String get imageUrl => _imageUrl;
+
   final fireAuth = FirebaseAuth.instance;
   final userService = UserFirerbaseService();
+
+  Future<void> saveUsersInfo() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    _email = pref.getString('email') ?? " No Email";
+    _name = pref.getString('name') ?? " No Name";
+    _surname = pref.getString('surname') ?? " No Surname";
+    _imageUrl = pref.getString('imageUrl') ?? " No imageUrl";
+  }
+
+  Future<void> updateUsersInfo(String newEmail, String newName,
+      String newSurname, File? newImageFIle) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    await pref.setString('email', newEmail);
+    await pref.setString('name', newName);
+    await pref.setString('surname', newSurname);
+
+    String imageUrl = _imageUrl;
+    if (newImageFIle != null) {
+      final storageRef = FirebaseStorage.instance
+          .ref()
+          .child('user_images')
+          .child('$newEmail.jpg');
+      await storageRef.putFile(newImageFIle);
+      imageUrl = await storageRef.getDownloadURL();
+      await pref.setString('userImageUrl', imageUrl);
+    }
+
+    _email = newEmail;
+    _name = newName;
+    _surname = newSurname;
+    _imageUrl = imageUrl;
+    notifyListeners();
+  }
 
   login(String email, String password) async {
     try {
@@ -34,7 +81,6 @@ class UserRegistrationController with ChangeNotifier {
         imageUrl: imageUrl,
         id: userdata.user!.uid,
       );
-      print("passwoerrd3defdercvr4c");
     } catch (e) {
       rethrow;
     }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:exam_project_with_providers/controllers/user_registration_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,22 +12,56 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    super.initState();
+    // Initialize user info when the screen is created
+    context.read<UserRegistrationController>().saveUsersInfo();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
       appBar: AppBar(
         title: const Text("Home Screen"),
         centerTitle: true,
         actions: [
           IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.notification_add_rounded)),
+            onPressed: () {},
+            icon: const Icon(Icons.notification_add_rounded),
+          ),
           IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              icon: const Icon(Icons.logout))
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(Icons.logout),
+          ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            Consumer<UserRegistrationController>(
+              builder: (context, userRegistrationController, child) {
+                return UserAccountsDrawerHeader(
+                  accountName: Text(userRegistrationController.name),
+                  accountEmail: Text(userRegistrationController.email),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage:
+                        userRegistrationController.imageUrl.isNotEmpty
+                            ? NetworkImage(userRegistrationController.imageUrl)
+                            : const NetworkImage(
+                                "https://static.vecteezy.com/system/resources/thumbnails/019/900/322/small/happy-young-cute-illustration-face-profile-png.png",
+                              ),
+                  ),
+                );
+              },
+            ),
+            // Add more items to the Drawer if needed
+          ],
+        ),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Home Screen'),
       ),
     );
   }
